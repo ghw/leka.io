1a0091636706c1d8eb03ddf044041c0935bd5f45
Ladislas de Toldi <ladislas@leka.io>
2015-06-19 10:09:09 +0200

    starts looking like something

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,23 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Discover Moti', lang: 'en' });
+  res.render('index', { title: 'Discover Brio', lang: 'en' });
+});
+
+router.get('/en', function(req, res) {
+  res.render('index', { title: 'Discover Brio', lang: 'en' });
+});
+
+router.get('/pre-order', function(req, res) {
+  res.render('index', { title: 'Pre-order Brio', lang: 'en' });
+});
+
+router.get('/fr', function(req, res) {
+  res.render('index', { title: 'Découvrez Brio', lang: 'fr' });
+});
+
+router.get('/fr/pre-order', function(req, res) {
+  res.render('index', { title: 'Précommandez Brio', lang: 'fr' });
 });
 
 module.exports = router;
