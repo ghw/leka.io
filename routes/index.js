16c91bafd8b3a0e4393cc041aea62d2d751c66f7
Anne Trema <annetrema01@gmail.com>
2015-08-21 09:21:17 +0200

    update anne

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,23 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Discover Brio', lang: 'en' });
+  res.render('index', { title: 'Discover {{productName}}', lang: 'en' });
 });
 
 router.get('/en', function(req, res) {
-  res.render('index', { title: 'Discover Brio', lang: 'en' });
+  res.render('index', { title: 'Discover {{productName}}', lang: 'en' });
 });
 
 router.get('/pre-order', function(req, res) {
-  res.render('index', { title: 'Pre-order Brio', lang: 'en' });
+  res.render('index', { title: 'Pre-order {{productName}}', lang: 'en' });
 });
 
 router.get('/fr', function(req, res) {
-  res.render('index', { title: 'Découvrez Brio', lang: 'fr' });
+  res.render('index', { title: 'Découvrez {{productName}}', lang: 'fr' });
 });
 
 router.get('/fr/pre-order', function(req, res) {
-  res.render('index', { title: 'Précommandez Brio', lang: 'fr' });
+  res.render('index', { title: 'Précommandez {{productName}}', lang: 'fr' });
 });
 
 module.exports = router;
