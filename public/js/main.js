753b755f4f47c39a5e690db79d467502ebdcc60f
AGirlWhoCode <annetrema01@gmail.com>
2015-09-11 19:08:55 +0200

    2 modals/1 for each type of button

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 // Modal Video
 $(function() {
-	$("#video-leka").on("change", function() {
+	$("#YTLink").on("change", function() {
 		if ($(this).is(":checked")) {
 			$("body").addClass("modal-open");
 		} else {
@@ -15,6 +15,58 @@ $(function() {
 	$(".modal-inner").on("click", function(e) {
 		e.stopPropagation();
 	});
+});
+
+// Scroll smoothly
+$('a[href^="#"]').on('click', function(event) {
+
+	var target = $( $(this).attr('href') );
+
+	if( target.length ) {
+		event.preventDefault();
+		$('html, body').animate({
+			scrollTop: target.offset().top
+		}, 1000);
+	}
+
+});
+
+//Modal Window Adopt a Leka
+$(function() {
+  $("#modal-1").on("change", function() {
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+  });
+
+  $(".modal-fade-screen, .modal-close").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
+
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
+  });
+});
+
+//Modal Window Join Us
+$(function() {
+  $("#modal-2").on("change", function() {
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+  });
+
+  $(".modal-fade-screen, .modal-close").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
+
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
+  });
 });
 
 // Scroll smoothly
